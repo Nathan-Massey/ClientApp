2af0aabde5df0d15650fec6bf8e2c68447fcc60a
unknown <JCDenton46@gmail.com>
2017-11-30 20:42:52 -0600

    Created our first tables in SQL.

---
+++ ClientApp/DataService/scripts/entries.sql
@@ -0,0 +1,23 @@
+﻿-- Data entry for Client table
+
+INSERT INTO Client (FirstName, LastName, Birthday)
+            VALUES ('Nathan', 'Massey', '1989-7-6')
+
+INSERT INTO Client (FirstName, LastName, Birthday)
+            VALUES ('Collin', 'Massey', '1992-4-7')
+
+INSERT INTO Client (FirstName, LastName, Birthday, Address)
+            VALUES ('Andrew', 'Luck', '1990-9-25', 'Indianapolis')
+
+-- ******************************************************************* --
+
+-- Data entry for Orders table
+
+INSERT INTO Orders (Description, Created, Shipped, Client_Id)
+			VALUES ('Towels', GETDATE(), 1, 1) -- Client Id 1 represents Nathan
+
+INSERT INTO Orders (Description, Created, Shipped, Client_Id)
+			VALUES ('Shirts', '2017-5-3', 1, 2)
+
+INSERT INTO Orders (Description, Created, Shipped, Client_Id)
+			VALUES ('Monitor', '2017-7-7', 1, 1)
---
+++ ClientApp/DataService/scripts/tables.sql
@@ -0,0 +1,24 @@
+﻿-- Create 2 tables , Client and Order
+
+-- Client
+
+CREATE TABLE Client
+(
+	Id INT PRIMARY KEY IDENTITY(1, 1), -- Primary Key
+	FirstName VARCHAR(max) NOT NULL,
+	LastName VARCHAR(max) NOT NULL,
+	Birthday DATE NOT NULL,
+	Address VARCHAR(max)
+)
+
+-- Order
+
+CREATE TABLE Orders
+(
+	Id INT PRIMARY KEY IDENTITY(1, 1),
+	Description VARCHAR(max) NOT NULL,
+	Created DATE NOT NULL,
+	Shipped BIT NOT NULL, -- Boolean
+
+	Client_Id INT -- Foreign Key from Client table
+)
