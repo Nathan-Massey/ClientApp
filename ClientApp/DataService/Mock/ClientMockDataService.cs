5970b4fd99329f673eb1447a7acc43028fea87a4
unknown <JCDenton46@gmail.com>
2017-12-11 12:29:41 -0600

    Added the OrderMockDataService. Still have one error.

---
+++ ClientApp/DataService/Mock/ClientMockDataService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataService.Interface;
+
+namespace DataService.Mock
+{
+    public class ClientMockDataService : IClientDataService
+    {
+
+        // Field
+        private List<Client> _mock;
+
+        // Constructor
+        public ClientMockDataService()
+        {
+            _mock = new List<Client>()
+            {
+                new Client() {Id = 1, Address= "123 Main Street", Birthday = new DateTime(2000, 03, 24), FirstName= "Nathan", LastName= "Massey"},
+                new Client() {Id = 2, Address= "345 2nd Street", Birthday = new DateTime(2002, 05, 06), FirstName= "Collin", LastName= "Massey"},
+                new Client() {Id = 3, Address= "678 3rd Street", Birthday = new DateTime(1997, 03, 24), FirstName= "Kawhi", LastName= "Leonard"},
+                new Client() {Id = 4, Address= "891 4th Street", Birthday = new DateTime(1995, 03, 24), FirstName= "Andrew", LastName= "Luck"},
+                new Client() {Id = 5, Address= "234 5th Street", Birthday = new DateTime(1980, 03, 24), FirstName= "Peyton", LastName= "Manning"},
+            };
+        }
+        public bool CreateClient(Client newClient)
+        {
+            _mock.Add(newClient);
+
+            return true;
+        }
+
+        public bool DeleteClient(int id)
+        {
+            var clientToBeDeleted = GetSingleClient(id);
+
+            _mock.Remove(clientToBeDeleted);
+
+            return true;
+        }
+
+        public List<Client> GetAllClients()
+        {
+            return _mock;
+        }
+
+        public Client GetSingleClient(int id)
+        {
+            return _mock.Single(client => client.Id == id);
+            
+        }
+
+        public bool UpdateClient(Client updatedClient)
+        {
+            var oldClient = GetSingleClient(updatedClient.Id);
+
+            _mock.Remove(oldClient);
+            _mock.Add(updatedClient);
+
+            return true;
+        }
+    }
+}
---
+++ ClientApp/DataService/Mock/OrderMockDataService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataService.Interface;
+
+namespace DataService.Mock
+{
+    class OrderMockDataService : IOrderDataService
+    {
+        // Field
+        private List<Order> _mock;
+
+        // Constructor
+        public OrderMockDataService()
+        {
+            _mock = new List<Order>()
+            {
+                 new Order() {Id = 1, Shipped = true, total = 100, Description = "Keyboard."},
+                 new Order() {Id = 2, Shipped = false, total = 200, Description = "Monitor."},
+                 new Order() {Id = 3, Shipped = true, total = 300, Description = "Gaming Console."},
+                 new Order() {Id = 4, Shipped = false, total = 400, Description = "TV."},
+                 new Order() {Id = 5, Shipped = true, total = 500, Description = "Laptop."},
+            };
+        }
+        public bool CreateOrder(Order newOrder)
+        {
+            _mock.Add(newOrder);
+
+            return true;
+        }
+
+        public bool DeleteOrder(int id)
+        {
+            var orderToBeDeleted = GetSingleOrder(id);
+
+            _mock.Remove(orderToBeDeleted);
+
+            return true;
+        }
+
+        public List<Order> GetAllOrders()
+        {
+            return _mock;
+        }
+
+        public Order GetSingleOrder(int id)
+        {
+            return _mock.Single(order => order.Id == id);
+        }
+
+        public bool UpdateOrder(Order updatedOrder)
+        {
+            var oldOrder = GetSingleOrder(updatedOrder.Id);
+
+            _mock.Remove(oldOrder);
+            _mock.Add(updatedOrder);
+
+            return true;
+        }
+    }
+}
